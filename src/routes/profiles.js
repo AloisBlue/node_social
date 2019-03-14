56e310b6d3c7a6c02e2281360c1ba927e6ffbde1
Alois Blue <blue@localhost.localdomain>
2019-03-14 15:15:27 +0300

    get profile working

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -4,11 +4,79 @@ import express from "express";
 import auth from "../middleware/auth";
 import User from "../models/User";
 import Profile from "../models/Profile";
-import validateProfileInput from "../validation/profile"
+import { validateProfileInput, validateExperienceInput, validateEducationInput } from "../validation/profile"
 
 const router = express.Router();
 
-// get profiles
+// get profile with handle
+// public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile
+    .findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "No profile found for this handle"
+        return res.status(404).json(errors)
+      } else {
+        res.status(200).json(profile)
+      }
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// get profile with user id
+// public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile
+    .findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "No profile by this user id"
+        return res.status(404).json(errors)
+      } else {
+        res.status(200).json(profile)
+      }
+    })
+    .catch(err => {
+      if (err) {
+        errors.noprofile = "No profile by this user id"
+        return res.status(404).json(errors)
+      }
+    });
+});
+
+// get all profiles
+// public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile
+    .find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (profiles.length == 0){
+        errors.noprofile = "There are no profiles"
+        return res.status(400).json(errors)
+      } else {
+        res.status(200).json(profiles)
+      }
+    })
+    .catch(err => {
+      if (err) {
+        errors.noprofile = "There are no profiles"
+        return res.status(404).json(errors)
+      }
+    });
+});
+
+// get your own profile
+// private
 router.get('/', auth, (req, res) => {
   const errors = {};
 
@@ -27,7 +95,8 @@ router.get('/', auth, (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
-// make profiles
+// make a profile
+// private
 router.post('/', auth, (req, res) => {
   const { errors, isValid } = validateProfileInput(req.body);
   if (!isValid) {
@@ -84,6 +153,173 @@ router.post('/', auth, (req, res) => {
         });
     }
   });
+});
+
+// add experience
+// private
+router.post('/experience', auth, (req, res) => {
+  const { errors, isValid } = validateExperienceInput(req.body);
+
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
+  Profile
+    .findOne({ user: req.user.user._id })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "You must have a profile to add experience"
+        return res.status(400).json(errors)
+      }
+
+      const newExperience = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      }
+
+      // add experience to existing profile
+      profile.experience.unshift(newExperience);
+      // save
+      profile
+        .save()
+        .then(profile => res.status(200).json(profile))
+        .catch(err => res.status(400).json(err));
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// add education
+// private
+router.post('/education', auth, (req, res) => {
+  const { errors, isValid } = validateEducationInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Profile
+    .findOne({ user: req.user.user._id })
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = "You must have a profile to add education background"
+        return res.status(400).json(errors)
+      }
+
+      const newEducation = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      }
+
+      // add education to existing profile
+      profile.education.unshift(newEducation);
+      profile
+      .save()
+      .then(profile => res.status(200).json(profile))
+      .catch(err => res.status(400).json(err));
+    })
+    .catch(err => res.status(400).json(err))
+});
+
+// delete experience
+// private
+router.delete('/experience/:exp_id', auth, (req, res) => {
+  const errors = {};
+
+  //check for profile
+  Profile
+    .findOne({ user: req.user.user._id })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for the user"
+        return res.status(404).json(errors);
+      } else if (profile.experience.length == 0) {
+        // return error if no experience by the provided id
+        errors.noexperience = "Either there is no experience for that id or it's already deleted"
+        return res.status(404).json(errors)
+      } else {
+        // make remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id)
+
+        profile.experience.splice(removeIndex, 1);
+        profile
+          .save()
+          .then(profile => res.status(200).json(profile))
+          .catch(err => res.status(400).json(err));
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// delete education
+// private
+router.delete('/education/:edu_id', auth, (req, res) => {
+  const errors = {};
+
+  // check for the profile
+  Profile
+    .findOne({ user: req.user.user._id })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for the user"
+        return res.status(404).json(errors)
+      } else if (profile.education.length == 0) {
+        errors.noeducation = "Either there is no education background for that id or it's already deleted"
+        return res.status(404).json(errors)
+      } else {
+        // make a remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        profile.education.splice(removeIndex, 1);
+        profile
+          .save()
+          .then(profile => res.status(200).json(profile))
+          .catch(err => res.status(400).json(err));
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// delete user account and profile
+// private
+router.delete('/', auth, (req, res) => {
+  const errors = {};
+
+  Profile
+    .findOneAndRemove({ user: req.user.user._id })
+    .then(() => {
+      // check if user exist when deleting
+      User
+      .findOne({ _id: req.user.user._id })
+      .then(user => {
+        if (!user) {
+          errors.user = "User already deleted"
+          return res.status(400).json(errors)
+        } else {
+          // continue and delete user
+          User
+            .findOneAndRemove({ _id: req.user.user._id })
+            .then(() => {
+              res.json({ message: "User account successfully deleted" })
+            })
+            .catch(err => res.json(err));
+        }
+      })
+      .catch(err => res.json(err));
+    })
+    .catch(err => res.json(err));
 });
 
 export default router;
--- src/validation/profile.js
+++ src/validation/profile.js
@@ -1,6 +1,7 @@
 import Validator from "validator";
 import isEmpty from "./isEmpty";
 
+// validate profile
 const validateProfileInput = data => {
   let errors = {};
 
@@ -66,4 +67,60 @@ const validateProfileInput = data => {
   };
 };
 
-export default validateProfileInput;
+// validate experience
+const validateExperienceInput = data => {
+  let errors = {};
+
+  data.title = !isEmpty(data.title) ? data.title : '';
+  data.company = !isEmpty(data.company) ? data.company : '';
+  data.from = !isEmpty(data.from) ? data.from : '';
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = 'Job title field is required';
+  }
+
+  if (Validator.isEmpty(data.company)) {
+    errors.company = 'Company field is required';
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = 'From date field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
+const validateEducationInput = data => {
+  let errors = {};
+
+  data.school = !isEmpty(data.school) ? data.school : '';
+  data.degree = !isEmpty(data.degree) ? data.degree : '';
+  data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : '';
+  data.from = !isEmpty(data.from) ? data.from : ''
+
+  if (Validator.isEmpty(data.school)) {
+    errors.school = 'School field is required'
+  }
+
+  if (Validator.isEmpty(data.degree)) {
+    errors.degree = 'Degree field is required'
+  }
+
+  if (Validator.isEmpty(data.fieldofstudy)) {
+    errors.fieldofstudy = 'Field of study field is required'
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = 'From field is required'
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
+export { validateProfileInput, validateExperienceInput, validateEducationInput };
